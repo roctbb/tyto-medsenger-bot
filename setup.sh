8c8b61d3698568013eee8e1dc9a0ab6f0e8918f8
roctbb <roctbb@gmail.com>
2023-02-06 21:22:34 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo cp agents_tyto.conf /etc/supervisor/conf.d/
 sudo cp agents_tyto_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
-sudo certbot --nginx -d tyto.medsenger.ru
+sudo certbot --nginx -d tyto.ai.medsenger.ru
 touch config.py
